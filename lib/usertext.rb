5260a1f29ea19582630ead683c07af697e10d4ab
Frankie Roberto <frankie@frankieroberto.com>
2011-03-06 16:56:58 +0000

    Pulling in code from previous repository

--- lib/usertext.rb
+++ lib/usertext.rb
@@ -1,11 +1,223 @@
 class Usertext
 
+
   def initialize(text)
     @string = text
   end
   
   def to_html
-    @string
+    html = @string
+
+    html = html.to_s
+    html = cleanup_newlines(html)
+    
+    # Character level markup
+    html = replace_typography(html)
+    html = insert_currency_symbols(html)
+    html = insert_fraction_symbols(html)
+    html = insert_roman_numerals(html)
+    html = insert_smileys(html)
+    html = insert_copyright_symbols(html)
+    
+    # Block level markup
+    html = html_escape(html)
+    html = mark_code(html)
+    html = auto_link(html)
+    html = auto_link_phone_numbers(html)
+    html = block_element_markup(html)
+
+    return html
   end
+  
+  private
+  
+    def block_element_markup(text)
+      new_text = Array.new
+      li_re     = /^\*\s(.*)$/
+      ol_re     = /^\d+[\.\)]\s(.*)$/ 
+      quote_re  = /^“(.*)”$/
+      code_re   = /^<code>.*<\/code>$/ 
+      text.split("\n\n").map do |block|
+        case block
+        when li_re
+          lines = block.gsub(li_re, '<li>\1</li>')
+          content_tag("ul", "\n" + lines + "\n")
+        when ol_re
+          lines = block.gsub(ol_re, '<li>\1</li>')
+          content_tag("ol", "\n" + lines + "\n")
+        when quote_re
+          content_tag("blockquote", content_tag("p", block.gsub(quote_re, '\1')))
+        when code_re
+          content_tag("div", block)
+        else        
+          content_tag("p", block)
+        end
+      end .join("\n\n")
+    
+    end
+    
+    def replace_typography(string)
+      string.
+      gsub(/\'(\d\ds)/, '’\1').             # replacing '30s with ’30s
+      gsub(/(\d+)'(\s)/, '\1′\2').          # replacing ' with single prime
+      gsub(/(\d+)"(\s)/, '\1″\2').          # replacing " with double prime
+      gsub(/(\d+)'''(\s)/, '\1‴\2').          # replacing ''' with triple prime
+      gsub(/(\d+)""(\s)/, '\1⁗\2').          # replacing "" with quadruple prime
+      gsub(/([^\s]+)\'(\s)/, '\1’\2').      # replacing ' with ’ (right single quote)
+      gsub(/(\s)\'([^\s])/, '\1‘\2').       # replacing ' with ‘ (left single quote)
+      gsub(/([^\s]+)\"/, '\1”').            # replacing " with ” (right double quote)
+      gsub(/\"([a-zA-Z])/, '“\1').             # replacing " with “ (left double quote)
+      gsub(/([^\s])\'([^\s])/, '\1’\2').    # replacing ' with ’ (apostrophe)
+    #   gsub(/(\s)\"(\s)/, '\1&#12291;\2').   # replacing " with 〃 (ditto mark)
+      gsub(/(\d+)x(\d+)/, '\1×\2').         # replacing x with multiplication char
+      gsub(/(\d{3,})-(\d{3,})/, '\1‒\2').           # replacing dash with figure dash
+      gsub(/(\d)-(\d)/, '\1–\2').           # replacing hypen-dash with en dash (for ranges)
+      gsub(/([A-Za-z\d])--([A-Za-z\d])/, '\1—\2').   # replacing double hypen-dash with em dash (parenthetical thought)
+      gsub(/([a-zA-Z])-([\sa-zA-Z])/, '\1‐\2'). # replacing hyphen-dash with hyphen char
+      gsub(/([\d])-([\d])/, '\1–\2').       # replacing - with en-dash
+      gsub(/([^\.])\.\.\.([^\.]|$)/, '\1…\2').# replacing ... with elipsis
+      gsub(/(\d) degrees c/, '\1℃').  # inserting typographic degrees character
+      gsub(/(\d) degrees f/, '\1℉').  # inserting typographic degrees character
+      gsub(/(\d) degrees (f)/, '\1°\2').  # inserting typographic degrees character
+      gsub(/(\s|^)No.(\s\d|$)/, '\1№\2').     # replace No. 3 with № 3   
+      gsub(/(\s|\A)c\/o(\s|$)/, '\1℅\2').     # replace c/o with ℅
+      gsub(/(\s|\A)a\/c(\s[A-Z]|$)/, '\1℀\2').     # replace a/c with ℀
+
+
+      to_s
+    end    
+
+
+    def insert_fraction_symbols(text)
+      text.
+      gsub(/(\s|^)1\/4(\s|$)/, '\1¼\2').     # replacing 1/4 with ¼
+      gsub(/(\s|^)1\/2(\s|$)/, '\1½\2').     # replacing 1/2 with ½
+      gsub(/(\s|^)3\/4(\s|$)/, '\1¾\2').     # replacing 3/4 with ¾      
+      gsub(/(\s|^)1\/3(\s|$)/, '\1⅓\2').     # replacing 1/3 with ⅓
+      gsub(/(\s|^)2\/3(\s|$)/, '\1⅔\2').     # replacing 2/3 with ⅔
+      gsub(/(\s|^)1\/5(\s|$)/, '\1⅕\2').     # replacing 1/5 with ⅕
+      gsub(/(\s|^)2\/5(\s|$)/, '\1⅖\2').     # replacing 2/5 with ⅖
+      gsub(/(\s|^)4\/5(\s|$)/, '\1⅖\2').     # replacing 4/5 with ⅘
+      gsub(/(\s|^)1\/6(\s|$)/, '\1⅙\2').     # replacing 1/6 with ⅙
+      gsub(/(\s|^)5\/6(\s|$)/, '\1⅖\2').     # replacing 5/6 with ⅚      
+      gsub(/(\s|^)1\/8(\s|$)/, '\1⅛\2').     # replacing 1/8 with ⅛           
+      gsub(/(\s|^)3\/8(\s|$)/, '\1⅜\2').     # replacing 3/8 with ⅜           
+      gsub(/(\s|^)5\/8(\s|$)/, '\1⅝\2').     # replacing 5/8 with ⅝           
+      gsub(/(\s|^)7\/8(\s|$)/, '\1⅞\2').     # replacing 7/8 with ⅞                     
+      to_s
+    end
+
+    def insert_currency_symbols(text)
+      text.
+      gsub(/(\s|^)GBP(\d+)/, '\1£\2').              # inserting Pound sign.
+      gsub(/(\s|^)USD?(\d+)/, '\1$\2').              # inserting Dollar sign.
+      gsub(/(\s|^)EUR(\d+)/, '\1€\2').              # inserting Euro sign.
+      gsub(/(\s|^)YEN(\d+)/, '\1¥\2').              # inserting Yen sign.
+      gsub(/(\s|^)ILS(\d+)/, '\1₪\2').              # inserting Israeli shekel symbol.
+      gsub(/(\s|^)GEL(\d+)/, '\1ლ\2').              # inserting Georgian lari symbol.
+      gsub(/(\s|^)AFN(\d+)/, '\1؋\2').              # inserting Afghan afghani symbol.
+      gsub(/(\s|^)ALL(\d+)/, '\1L\2').              # inserting Albanian lek symbol.
+      gsub(/(\s|^)DZD(\d+)/, '\1د.ج\2').              # inserting Algerian dinar symbol.
+      gsub(/(\s|^)PHP(\d+)/, '\1₱\2').              # inserting Philippine peso symbol.
+      gsub(/(\s|^)UAH(\d+)/, '\1₴\2').              # inserting Ukrainian hryvnia symbol.
+      to_s
+
+    end  
+
+    def cleanup_newlines(text)
+      text.gsub(/\r\n?/, "\n")                    # \r\n and \r -> \n
+    end
+
+    def auto_link_phone_numbers(text)
+      text.gsub(/(\s|^)((0|\+44)\d{10,10})\b/) do 
+        text = $1 + "<a href=\"tel:" + $2 + "\">" + $2 + "</a>"
+      end
+    end    
+    
+    def mark_code(text)
+      text.
+      gsub(/(^&lt;[a-zA-Z]+.*$|&lt;[a-zA-Z]+.*&gt;)/) do 
+        "<code>" + $1 + "</code>"
+      end
+    end
+
+    def auto_link(text)
+      auto_link_urls(text)
+    end
+
+    def auto_link_urls(text)
+
+      auto_link_re = %r{
+       (                          # leading text
+         <\w+.*?>|                # leading HTML tag, or
+         [^=!:'"/]|               # leading punctuation, or 
+         ^                        # beginning of line
+       )
+       (https?://|ftp://)              # protocol spec
+
+       (
+         [-\w]+                   # subdomain or domain
+         (?:\.[-\w]+)*            # remaining subdomains or domain
+         (?::\d+)?                # port
+         (?:/(?:(?:[~\w\+@%-]|(?:[,.;:][^\s$]))+)?)* # path
+         (?:\?[\w\+@%&=.;-]+)?     # query string
+         (?:\#[\w\-\/]*)?           # trailing anchor
+       )(([[:punct:]]|\s|<|$))       # trailing text
+      }x
+
+      text.gsub(auto_link_re) do
+        all, a, b, c, d = $&, $1, $2, $3, $5
+
+        text = a + "<a href=\"" + b + c + "\">" + truncate_in_middle(c, 40) + "</a>" + $5
+      end
+    end
+
+    # This helper is like the trunctate() method included in Rails core, but truncates
+    # in the middle of the string rather than at the end. This is useful for things like
+    # URLs, which it is sometimes helpful to show the beginning and the end.
+    def truncate_in_middle(text, length = 30, truncate_string = "...")
+      if text
+        l = (length - truncate_string.length) / 2
+        return text if length > text.length
+        return text[0...l] + truncate_string + text[-l..-1]
+      end
+    end      
+
+    def insert_roman_numerals(text)
+      text.
+      gsub(/(\s|^)II([\s\.]|$)/, '\1Ⅱ\2').       # Replace II with Ⅱ (roman numeral)
+      gsub(/(\s|^)III([\s\.]|$)/, '\1Ⅲ\2').      # Replace III with Ⅲ (roman numeral)
+      gsub(/(\s|^)IV([\s\.]|$)/, '\1Ⅳ\2').      # Replace IV with Ⅳ (roman numeral)
+      gsub(/(\s|^)V([\s\.]|$)/, '\1Ⅴ\2').      # Replace V with Ⅴ (roman numeral)
+      gsub(/(\s|^)VI([\s\.]|$)/, '\1Ⅵ\2').      # Replace VI with Ⅵ (roman numeral)
+      gsub(/(\s|^)VII([\s\.]|$)/, '\1Ⅶ\2').      # Replace VII with Ⅶ (roman numeral)
+      gsub(/(\s|^)VIII([\s\.]|$)/, '\1Ⅷ\2').      # Replace VIII with Ⅷ (roman numeral)
+      gsub(/(\s|^)IX([\s\.]|$)/, '\1Ⅸ\2').      # Replace IX with Ⅸ (roman numeral)
+      gsub(/(\s|^)X([\s\.]|$)/, '\1Ⅹ\2').      # Replace X with Ⅹ (roman numeral)
+      to_s
+    end
+
+    def insert_smileys(text)
+      text.
+      gsub(/(\s|^):-?\)(\s|$)/, '\1☺\2').       # Replace :-) or :) with ☺
+      gsub(/(\s|^):-?\((\s|$)/, '\1☹\2').       # Replace :-( or :( with ☹
+      to_s    
+    end  
+
+    def insert_copyright_symbols(text)
+      text.
+      gsub(/\(R\)([\W]|$)/, 'Ⓡ\1').     # inserting Registered trademark sign.        
+      gsub(/\(c\)([\sA-Za-z]|$)/, '©\1').     # inserting Copyright sign.
+      to_s
+    end    
+    
+    def content_tag(tagname, contents, options = {})      
+      "<" + tagname + ">" + contents + "</" + tagname + ">"
+    end
+    
+    def html_escape(s)
+      s.to_s.gsub(/&/, "&amp;").gsub(/\"/, "&quot;").gsub(/>/, "&gt;").gsub(/</, "&lt;")
+    end
+    
   
 end
--- test/test_usertext.rb
+++ test/test_usertext.rb
@@ -8,4 +8,226 @@ class TestUsertext < Test::Unit::TestCase
     
   end
 
+  should "Insert paragraph tags" do
+    assert_equal("<p>Hello!</p>", Usertext.new("Hello!").to_html)
+  end
+  
+  should "Insert two paragraph tags" do
+    assert_equal("<p>Hello!</p>\n\n<p>Wow!</p>", Usertext.new("Hello!\n\nWow!").to_html)
+  end
+  
+  should "Insert three paragraph tags" do
+    assert_equal("<p>Hello!</p>\n\n<p>Wow!</p>\n\n<p>Woop!</p>", 
+      Usertext.new("Hello!\n\nWow!\n\nWoop!").to_html)
+  end
+  
+  should "Insert four paragraph tags" do
+    assert_equal("<p>Hello!</p>\n\n<p>Wow!</p>\n\n<p>Woop!</p>\n\n<p>Pow!</p>", 
+      Usertext.new("Hello!\n\nWow!\n\nWoop!\n\nPow!").to_html)
+  end
+
+  def test_elipsis_insertion
+    assert_equal "<p>I eat… worms!</p>", Usertext.new("I eat... worms!").to_html
+  end
+
+  def test_elipsis_insertion_at_end_of_string
+    assert_equal "<p>I want to…</p>", Usertext.new("I want to...").to_html
+  end
+
+  def test_insert_typographic_apostrophe
+    assert_equal "<p>Jamie’s house</p>", Usertext.new("Jamie's house").to_html
+  end
+  
+  def test_insert_typographic_apostrophe_in_decade_reference
+    assert_equal "<p>It was acceptable in the ’80s.</p>", Usertext.new("It was acceptable in the '80s.").to_html
+  end
+  
+  def test_insert_typographic_double_quotes
+    assert_equal "<p>She said “hello”.</p>", Usertext.new("She said \"hello\".").to_html
+  end
+  
+  def test_insert_typographic_single_quotes
+    assert_equal "<p>She said ‘hello’.</p>", Usertext.new("She said 'hello'.").to_html
+  end  
+  
+  def test_insert_multiplication_character
+    assert_equal "<p>2×2=4</p>", Usertext.new("2x2=4").to_html
+  end
+  
+  def test_insert_figure_dash
+    assert_equal "<p>Telephone 867‒5309.</p>", Usertext.new("Telephone 867-5309.").to_html
+  end
+  
+  def test_insert_en_dash
+    assert_equal "<p>For ages 3–5.</p>", Usertext.new("For ages 3-5.").to_html
+  end
+  
+  def test_insert_em_dash
+    assert_equal "<p>I am not sure—although I really should be—what to do.</p>", Usertext.new("I am not sure--although I really should be--what to do.").to_html
+  end
+  
+
+
+  def test_insert_degrees_symbol
+    assert_equal "<p>It is 20℃.</p>", Usertext.new("It is 20 degrees c.").to_html
+    assert_equal "<p>It is 20℉.</p>", Usertext.new("It is 20 degrees f.").to_html
+  end
+
+  def test_insert_pound_symbol
+    assert_equal "<p>That costs £30.</p>", Usertext.new("That costs GBP30.").to_html
+  end
+
+  def test_insert_dollar_symbol
+    assert_equal "<p>That costs $30.</p>", Usertext.new("That costs USD30.").to_html
+    assert_equal "<p>That costs $30.</p>", Usertext.new("That costs US30.").to_html
+    assert_equal "<p>Item code PTSTUSD7201.</p>", Usertext.new("Item code PTSTUSD7201.").to_html
+  end
+  
+  def test_insert_euro_symbol
+    assert_equal "<p>That costs €30.</p>", Usertext.new("That costs EUR30.").to_html
+  end
+
+  def test_insert_yen_symbol
+    assert_equal "<p>That costs ¥30.</p>", Usertext.new("That costs YEN30.").to_html
+  end
+  
+  def test_insert_sheqel_sign_symbol
+    assert_equal "<p>That costs ₪30.</p>", Usertext.new("That costs ILS30.").to_html
+  end
+  
+  
+  def test_insert_copyright_symbol
+    assert_equal "<p>©Frankie Roberto</p>", Usertext.new("(c)Frankie Roberto").to_html
+    assert_equal "<p>© Frankie Roberto</p>", Usertext.new("(c) Frankie Roberto").to_html
+    assert_equal "<p>This text is in ©</p>", Usertext.new("This text is in (c)").to_html
+  end
+
+  
+  def test_insert_suspended_hyphen
+    assert_equal "<p>nineteenth‐ and twentieth‐century writers</p>", Usertext.new("nineteenth- and twentieth-century writers").to_html
+  end
+  
+  def test_insert_hyphen
+    assert_equal "<p>This is a real‐world example.</p>", Usertext.new("This is a real-world example.").to_html
+  end  
+  
+  def test_auto_link_phone_numbers
+    assert_equal "<p>Phone me on <a href=\"tel:+447736111111\">+447736111111</a></p>", Usertext.new("Phone me on +447736111111").to_html
+    assert_equal "<p>Phone me on <a href=\"tel:+447736111111\">+447736111111</a>.</p>", Usertext.new("Phone me on +447736111111.").to_html
+    assert_equal "<p>Phone me on <a href=\"tel:07736111111\">07736111111</a></p>", Usertext.new("Phone me on 07736111111").to_html
+    assert_equal "<p>Phone me on <a href=\"tel:07736111111\">07736111111</a>.</p>", Usertext.new("Phone me on 07736111111.").to_html
+  end
+  
+  def test_insert_numero_character
+    assert_equal "<p>I live at № 4 Privet Drive</p>", Usertext.new("I live at No. 4 Privet Drive").to_html
+    assert_equal "<p>Tell me your №</p>", Usertext.new("Tell me your No.").to_html
+    assert_equal "<p>№ 3 is the winner!</p>", Usertext.new("No. 3 is the winner!").to_html
+  end
+
+  def test_care_of_character
+    assert_equal "<p>Send it to me ℅ my company.</p>", Usertext.new("Send it to me c/o my company.").to_html
+  end
+
+  
+  def test_code_tag
+    assert_equal "<p>Don’t use the <code>&lt;blink&gt;</code> tag.</p>", Usertext.new("Don't use the <blink> tag.").to_html
+    assert_equal "<p>4 &lt; 7 &gt; 1</p>", Usertext.new("4 < 7 > 1").to_html
+  end
+  
+  def test_block_of_code
+    assert_equal "<p>Use this code:</p>\n\n<div><code>&lt;html&gt;</code></div>",
+    Usertext.new("Use this code:\n\n<html>").to_html
+  end
+  
+  def test_mark_list
+    assert_equal "<ul>\n<li>Milk</li>\n<li>Eggs</li>\n<li>Flour</li>\n</ul>", Usertext.new("* Milk\n* Eggs\n* Flour").to_html
+  end
+  
+  def test_mark_lists
+    assert_equal "<p>List:</p>\n\n<ul>\n<li>apples</li>\n<li>pears</li>\n</ul>\n\n<p>And forget milk!</p>", Usertext.new("List:\n\n* apples\n* pears\n\nAnd forget milk!").to_html
+  end
+
+  def test_mark_lists_2
+    assert_equal "<p>List:</p>\n\n<ul>\n<li>apples</li>\n<li>pears</li>\n</ul>", Usertext.new("List:\n\n* apples\n* pears").to_html
+  end
+  
+  def test_mark_ordered_list
+    assert_equal "<p>Method:</p>\n\n<ol>\n<li>Stir</li>\n<li>Bake</li>\n<li>Lick spoon</li>\n</ol>", Usertext.new("Method:\n\n1. Stir\n2. Bake\n3. Lick spoon").to_html
+    assert_equal "<p>Method:</p>\n\n<ol>\n<li>Stir</li>\n<li>Bake</li>\n<li>Lick spoon</li>\n</ol>", Usertext.new("Method:\n\n1) Stir\n2) Bake\n3) Lick spoon").to_html
+  end
+  
+  def test_nothing
+    assert_equal("", Usertext.new("").to_html)
+  end
+
+  def test_auto_link_ftp_url
+    assert_equal "<p><a href=\"ftp://test.com\">test.com</a></p>", Usertext.new("ftp://test.com").to_html
+  end
+  
+  def test_auto_link_url
+    assert_equal "<p><a href=\"http://test.com\">test.com</a></p>", Usertext.new("http://test.com").to_html
+  end
+
+  def test_auto_link_truncated_url
+    assert_equal "<p><a href=\"http://thisisareallyreallyreallyreallyreallylongurl.com\">thisisareallyreall...yreallylongurl.com</a></p>", Usertext.new("http://thisisareallyreallyreallyreallyreallylongurl.com").to_html
+  end
+  
+  def test_one_quarter_symbol
+    assert_equal "<p>I have ¼ hour left.</p>",
+    Usertext.new("I have 1/4 hour left.").to_html
+  end
+
+  def test_one_half_symbol
+    assert_equal "<p>I have ½ hour left.</p>",
+    Usertext.new("I have 1/2 hour left.").to_html
+  end
+  
+  def test_threw_quarters_symbol
+    assert_equal "<p>I have ¾ hour left.</p>", Usertext.new("I have 3/4 hour left.").to_html
+    assert_equal "<p>Give me ¾</p>", Usertext.new("Give me 3/4").to_html
+    assert_equal "<p>I am on 123/4567.</p>", Usertext.new("I am on 123/4567.").to_html
+  end  
+  
+  def test_insert_registered_trademark_symbol
+    assert_equal "<p>I like LegoⓇ.</p>", Usertext.new("I like Lego(R).").to_html
+    assert_equal "<p>I like LegoⓇ</p>", Usertext.new("I like Lego(R)").to_html
+    assert_equal "<p>I like LegoⓇ bricks.</p>", Usertext.new("I like Lego(R) bricks.").to_html
+    assert_equal "<p>x = 2(R)n</p>", Usertext.new("x = 2(R)n").to_html
+  end
+
+  
+  def test_blockquote
+    assert_equal "<p>She said:</p>\n\n<blockquote><p>I love you.</p></blockquote>", Usertext.new("She said:\n\n\"I love you.\"").to_html
+  end
+
+
+  def test_insert_roman_numerals
+    assert_equal "<p>The year Ⅱ.</p>", Usertext.new("The year II.").to_html
+    assert_equal "<p>Ⅱ green bottles.</p>", Usertext.new("II green bottles.").to_html
+    assert_equal "<p>The year Ⅲ.</p>", Usertext.new("The year III.").to_html
+    assert_equal "<p>Ⅲ green bottles.</p>", Usertext.new("III green bottles.").to_html
+    assert_equal "<p>The year Ⅳ.</p>", Usertext.new("The year IV.").to_html
+    assert_equal "<p>Ⅳ green bottles.</p>", Usertext.new("IV green bottles.").to_html
+    assert_equal "<p>The year Ⅴ.</p>", Usertext.new("The year V.").to_html
+    assert_equal "<p>Ⅴ green bottles.</p>", Usertext.new("V green bottles.").to_html
+    assert_equal "<p>The year Ⅵ.</p>", Usertext.new("The year VI.").to_html
+    assert_equal "<p>Ⅵ green bottles.</p>", Usertext.new("VI green bottles.").to_html
+    assert_equal "<p>The year Ⅶ.</p>", Usertext.new("The year VII.").to_html
+    assert_equal "<p>Ⅶ green bottles.</p>", Usertext.new("VII green bottles.").to_html
+    assert_equal "<p>The year Ⅷ.</p>", Usertext.new("The year VIII.").to_html
+    assert_equal "<p>Ⅷ green bottles.</p>", Usertext.new("VIII green bottles.").to_html
+    assert_equal "<p>The year Ⅸ.</p>", Usertext.new("The year IX.").to_html
+    assert_equal "<p>Ⅸ green bottles.</p>", Usertext.new("IX green bottles.").to_html
+    assert_equal "<p>The year Ⅹ.</p>", Usertext.new("The year X.").to_html
+    assert_equal "<p>Ⅹ green bottles.</p>", Usertext.new("X green bottles.").to_html
+  end
+
+  def test_insert_smiley_characters
+    assert_equal "<p>Yay! ☺</p>", Usertext.new("Yay! :-)").to_html
+    assert_equal "<p>Yay! ☺</p>", Usertext.new("Yay! :)").to_html    
+    assert_equal "<p>No! ☹</p>", Usertext.new("No! :-(").to_html
+    assert_equal "<p>No! ☹</p>", Usertext.new("No! :(").to_html    
+  end
+
+
 end
